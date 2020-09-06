79c79e9cd2d232e91cdf93818d1a5eaad08f2adc
Arafat M <19172+arafatm@users.noreply.github.com>
2020-09-06 16:53:21 -0700

    refactor tests with a `t.Helper()`

--- code/hello/hello_test.go
+++ code/hello/hello_test.go
@@ -4,21 +4,25 @@ import "testing"
 
 func TestHello(t *testing.T) {
 
-	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
-		want := "Hello, Chris"
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper() // declare this is a helper method
 
 		if got != want {
 			t.Errorf("got %q want %q", got, want)
 		}
+	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
+
+		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, world' by default", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, world"
 
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 }
