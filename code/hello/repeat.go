5b85a7c800585d1d1b73b80602aa73c3a0cbe42a
Arafat M <19172+arafatm@users.noreply.github.com>
2020-09-07 15:36:10 -0700

    refactor with `+=` assignment

--- code/hello/repeat.go
+++ code/hello/repeat.go
@@ -1,10 +1,12 @@
 package learngowithtests
 
+const repeatCount = 5
+
 // Repeat a string 5 times
 func Repeat(input string) string {
 	var repeated string
-	for i := 0; i < 5; i++ {
-		repeated = repeated + input
+	for i := 0; i < repeatCount; i++ {
+		repeated += input
 	}
 	return repeated
 }
