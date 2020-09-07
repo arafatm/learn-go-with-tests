0d3913e7ce196dca1724b1a4fca20b3853bfc167
Arafat M <19172+arafatm@users.noreply.github.com>
2020-09-06 17:31:56 -0700

    sample `Example` that can be verified by running `godoc`

--- code/hello/adder_test.go
+++ code/hello/adder_test.go
@@ -1,6 +1,13 @@
 package learngowithtests
 
 import "testing"
+import "fmt"
+
+func ExampleAdd() {
+	sum := Add(1, 5)
+	fmt.Println(sum)
+	// Output: 6
+}
 
 func TestAdder(t *testing.T) {
 	sum := Add(2, 2)
