803e3f470f4e8e65cae5f5cc98ae3a6b3d93f332
Arafat M <19172+arafatm@users.noreply.github.com>
2020-09-06 17:00:28 -0700

    Add language support with `if` statements

--- code/hello/hello.go
+++ code/hello/hello.go
@@ -3,14 +3,25 @@ package main
 import "fmt"
 
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bon jour, "
 
-func Hello(name string) string {
+func Hello(name string, lang string) string {
 	if name == "" {
 		name = "world"
 	}
+
+	if lang == "spanish" {
+		return spanishHelloPrefix + name
+	}
+
+	if lang == "french" {
+		return frenchHelloPrefix + name
+	}
+
 	return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello(""))
+	fmt.Println(Hello("", ""))
 }
--- code/hello/hello_test.go
+++ code/hello/hello_test.go
@@ -13,16 +13,28 @@ func TestHello(t *testing.T) {
 	}
 
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "English")
 		want := "Hello, Chris"
 
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, world' by default", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, world"
 
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Francis", "french")
+		want := "Bon jour, Francis"
 		assertCorrectMessage(t, got, want)
 	})
 }
