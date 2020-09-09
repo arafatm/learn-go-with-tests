d6ca7498ba6f98454d841972c6d4d7bfb7f248bf
Arafat M <19172+arafatm@users.noreply.github.com>
2020-09-09 16:11:55 -0700

    Using `float64`, `Perimeter` func and test

---
+++ code/hello/perimeter.go
@@ -0,0 +1,5 @@
+package learngowithtests
+
+func Perimeter(width float64, height float64) float64 {
+	return 2 * (width + height)
+}
---
+++ code/hello/perimeter_test.go
@@ -0,0 +1,8 @@
+package learngowithtests
+
+import "fmt"
+
+func ExamplePerimeter() {
+	fmt.Println(Perimeter(10.2, 10.0))
+	// Output: 40.4
+}
