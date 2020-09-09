ced1f83457545b21aeb09fa9a8ea309eb40ddd52
Arafat M <19172+arafatm@users.noreply.github.com>
2020-09-09 16:51:05 -0700

    Using structs Rectangle not found struct not found

--- code/hello/shapes.go
+++ code/hello/shapes.go
@@ -1,9 +1,15 @@
 package learngowithtests
 
-func Perimeter(width float64, height float64) float64 {
-	return 2 * (width + height)
+type Rectangle struct {
+	width  float64
+	height float64
 }
 
-func Area(width float64, height float64) float64 {
-	return width * height
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.width + rectangle.height)
+	// test
+}
+
+func Area(rectangle Rectangle) float64 {
+	return rectangle.width * rectangle.height
 }
--- code/hello/shapes_test.go
+++ code/hello/shapes_test.go
@@ -3,11 +3,11 @@ package learngowithtests
 import "fmt"
 
 func ExamplePerimeter() {
-	fmt.Println(Perimeter(10.2, 10.0))
-	// Output: 40.4
+	fmt.Println(Perimeter(Rectangle{10.0, 5}))
+	// Output: 30
 }
 
 func ExampleArea() {
-	fmt.Println(Area(12.0, 6.0))
+	fmt.Println(Area(Rectangle{12.0, 6.0}))
 	// Output: 72
 }
