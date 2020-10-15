e4f7fd79ef609cfab7a5bdd0fe4edd4f10954c1f
Arafat M <19172+arafatm@users.noreply.github.com>
2020-10-14 19:51:13 -0700

    Shapes (dont forget the return on a func)

--- code/hello/shapes_test.go
+++ code/hello/shapes_test.go
@@ -8,25 +8,22 @@ func ExamplePerimeterRectangle() {
 	// Output: 30
 }
 
-func ExampleAreaRectangle() {
-	r := Rectangle{12, 6}
-	fmt.Println(CheckArea(r))
-	// Output: 72
-}
 func ExamplePerimeterCircle() {
 	c := Circle{10}
 	fmt.Println(c.Perimeter())
 	// Output: 62.83185307179586
 }
 
+func checkArea(shape Shape) float64 {
+	return shape.Area()
+}
+
 func ExampleAreaCircle() {
-	c := Circle{10}
-	fmt.Println(CheckArea(c))
+	fmt.Println(checkArea(Circle{10}))
 	// Output: 314.1592653589793
 }
 
-func CheckArea(shape.Shape) {
-	shape.Area
+func ExampleAreaRectangle() {
+	fmt.Println(checkArea(Rectangle{12, 6}))
+	// Output: 72
 }
- 
-  
