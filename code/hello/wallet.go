d5aba522664517f795dd9e0e1e0de31f3798f6e2
Sally Lehman <rothsa@gmail.com>
2020-10-14 20:51:52 -0700

    amazing code

---
+++ code/hello/wallet.go
@@ -0,0 +1,25 @@
+package learngowithtests
+
+import "fmt"
+
+type Bitcoin int
+
+type Wallet struct {
+	balance Bitcoin
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
+	w.balance += amount
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) {
+	w.balance -= amount
+}
+
+func (w *Wallet) Balance() Bitcoin {
+	return w.balance
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
