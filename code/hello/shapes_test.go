ad1e6409c5d076ce13ea8ffa3083f61fc1f6b7f6
Arafat M <19172+arafatm@users.noreply.github.com>
2020-10-14 18:53:04 -0700

    shapes functions for Circle

--- code/hello/shapes_test.go
+++ code/hello/shapes_test.go
@@ -2,12 +2,21 @@ package learngowithtests
 
 import "fmt"
 
-func ExamplePerimeter() {
+func ExamplePerimeterRectangle() {
 	fmt.Println(Perimeter(Rectangle{10.0, 5}))
 	// Output: 30
 }
 
-func ExampleArea() {
+func ExampleAreaRectangle() {
 	fmt.Println(Area(Rectangle{12.0, 6.0}))
 	// Output: 72
+}
+func ExamplePerimeterCircle() {
+	fmt.Println(Perimeter(Circle{10.0}))
+	// Output: 62.8
+}
+
+func ExampleAreaCircle() {
+	fmt.Println(Area(Circle{10}))
+	// Output: 314
 }
