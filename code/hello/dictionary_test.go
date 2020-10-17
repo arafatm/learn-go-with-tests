f1c461fc2ad4fa2b64205c75dc73759e49c0a774
Arafat M <19172+arafatm@users.noreply.github.com>
2020-10-16 20:27:15 -0700

    Check if word exists in Dictionary prior to adding https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/maps#refactor-3

--- code/hello/dictionary_test.go
+++ code/hello/dictionary_test.go
@@ -1,10 +1,10 @@
 package learngowithtests
 
 import "testing"
-import "fmt"
 
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just a test"}
+
 	t.Run("known word", func(t *testing.T) {
 		got, _ := dictionary.Search("test")
 		want := "this is just a test"
@@ -14,24 +14,39 @@ func TestSearch(t *testing.T) {
 
 	t.Run("unknown word", func(t *testing.T) {
 		_, got := dictionary.Search("unknown")
- 		
+
 		assertError(t, got, ErrNotFound)
 	})
+
 }
 
 func TestAdd(t *testing.T) {
-	dictionary := Dictionary{}
-	word := "test"
-	definition := "this is just a test"
+	t.Run("new word", func(t *testing.T) {
+		dictionary := Dictionary{}
+		word := "test"
+		definition := "this is just a test"
 
-	dictionary.Add(word, definition)
-	assertDefinition(t, dictionary, word, definition)
+		err := dictionary.Add(word, definition)
+
+		assertError(t, err, nil)
+		assertDefinition(t, dictionary, word, definition)
+	})
+
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{word: definition}
+		err := dictionary.Add(word, "new test")
+
+		assertError(t, err, ErrWordExists)
+		assertDefinition(t, dictionary, word, definition)
+	})
 }
 
 func assertDefinition(t *testing.T, dictionary Dictionary, word, definition string) {
 	t.Helper()
+
 	got, err := dictionary.Search(word)
-	fmt.Println(&dictionary, &word, &definition)
 	if err != nil {
 		t.Fatal("should find added word:", err)
 	}
@@ -45,7 +60,14 @@ func assertError(t *testing.T, got, want error) {
 	t.Helper()
 
 	if got != want {
-             t.Errorf("got error %q want %q", got, want)
+		t.Errorf("got error %q want %q", got, want)
+	}
+
+	if got == nil {
+		if want == nil {
+			return
+		}
+		t.Fatal("expected to get an error")
 	}
 }
 
