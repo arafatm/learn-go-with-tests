6e5fff2b81c84bdffee854a6e43e4eebdc5f96b4
Arafat M <19172+arafatm@users.noreply.github.com>
2020-10-17 19:31:49 -0700

    countdown actually counts down from 3..2..1..Go!

--- code/countdown/countdown.go
+++ code/countdown/countdown.go
@@ -6,8 +6,14 @@ import (
 	"os"
 )
 
+const finalWord = "Go!"
+const countdownStart = 3
+
 func Countdown(out io.Writer) {
-	fmt.Fprint(out, "3")
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprintln(out, i)
+	}
+	fmt.Fprint(out, finalWord)
 }
 
 func main() {
