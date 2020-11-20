61f28f3baa0082198b595753770aa1ea08429a84
Arafat M <19172+arafatm@users.noreply.github.com>
2020-11-19 20:42:43 -0800

    pass initial reflection test https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/reflection#write-the-test-first-1

--- code/reflection/walk.go
+++ code/reflection/walk.go
@@ -1,7 +1,8 @@
 package main
 
-import "fmt"
+func walk(x interface{}, fn func(input string)) {
+	fn("I still can't believe South Korea beat Germany 2-0 to put them last in their group")
+}
 
 func main() {
-	fmt.Println("vim-go")
 }
