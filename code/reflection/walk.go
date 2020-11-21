69d18fe939b5140ef06a32bc52413f49b9679de1
Sally Lehman <rothsa@gmail.com>
2020-11-20 18:55:05 -0800

    reflection https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/reflection#refactor-3

--- code/reflection/walk.go
+++ code/reflection/walk.go
@@ -1,7 +1,23 @@
 package main
 
+import (
+	"reflect"
+	"fmt"
+)
+
 func walk(x interface{}, fn func(input string)) {
-	fn("I still can't believe South Korea beat Germany 2-0 to put them last in their group")
+	val := reflect.ValueOf(x)
+
+	for i:=0; i<val.NumField(); i++ {
+		field := val.Field(i)
+			fmt.Println(field.Kind())
+		if field.Kind() == reflect.String {
+			fn(field.String())
+		}
+		if field.Kind() == reflect.Struct {
+			walk(field.Interface(), fn)
+		}
+	}
 }
 
 func main() {
--- code/reflection/walk_test.go
+++ code/reflection/walk_test.go
@@ -1,20 +1,72 @@
 package main
 
-import "testing"
+import (
+	"testing"
+	"reflect"
+	"fmt"
+)
+
+type Person struct {
+	Name string
+	Profile Profile
+}
+
+type Profile struct {
+	Age int
+	City string
+}
 
 func TestWalk(t *testing.T) {
-	expected := "Chris"
-	var got []string
 
-	x := struct {
+	cases := []struct{
 		Name string
-	}{expected}
+		Input interface{}
+		ExpectedCalls []string
+	} {
+		{
+			"Struct with one string filed",
+			struct {
+				Name string
+			}{"Chris"},
+			[]string{"Chris"},
+		},
+		{
+			"Struct with two string fields",
+			struct {
+				Name string
+				City string
+			}{"Chris", "London"},
+			[]string{"Chris", "London"},
+		},
+		{
+			"String with non string field",
+			struct {
+				Name string
+				Age int
+			}{"Chris", 33},
+			[]string{"Chris"},
+		},
+		{
+			"Nested fields",
+			Person{
+				"Chris",
+				Profile{33, "London"},
+			},
+			[]string{"Chris", "London"},
+		},
+	}
 
-	walk(x, func(input string) {
-		got = append(got, input)
-	})
+	for _, test := range cases {
+		t.Run(test.Name, func(t *testing.T) {
+			fmt.Printf("test.Name = %v\n", test.Name)
+			var got []string
+			walk(test.Input, func(input string) {
+				got = append(got, input)
+			})
 
-	if len(got) != 1 {
-		t.Errorf("wrong number of function calls, got %d want %d", len(got), 1)
+			if !reflect.DeepEqual(got, test.ExpectedCalls) {
+				t.Errorf("got %v, want %v", got, test.ExpectedCalls)
+			}
+		})
 	}
 }
